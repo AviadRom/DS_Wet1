bfcfea548f2b9486c1a3507a4dd50367dd344229
Aviad Rom <b2faviad@gmail.com>
2013-05-03 15:07:00 +0300

    null reference in library1.cpp fixed

--- Mivne1/library1.cpp
+++ Mivne1/library1.cpp
@@ -15,34 +15,58 @@ void*       Init(){
 }
 
 StatusType AddCourse(void *DS, int courseID, int size){
+    if (DS == NULL){
+        return INVALID_INPUT;
+    }
     return (((Statistics*)DS)->AddCourse(courseID,size));
 }
 
 StatusType RemoveCourse(void *DS,int courseID){
+    if (DS == NULL){
+        return INVALID_INPUT;
+    }
     return (((Statistics*)DS)->RemoveCourse(courseID));
 }
 
 StatusType AddStudent(void *DS, int studentID){
+    if (DS == NULL){
+        return INVALID_INPUT;
+    }
     return (((Statistics*)DS)->AddStudent(studentID));
 }
 
 StatusType RemoveStudent(void *DS,int studentID){
+    if (DS == NULL){
+        return INVALID_INPUT;
+    }
     return (((Statistics*)DS)->RemoveStudent(studentID));
 }
 
 StatusType TakeCourse(void *DS, int studentID, int courseID){
+    if (DS == NULL){
+        return INVALID_INPUT;
+    }
     return (((Statistics*)DS)->TakeCourse(studentID, courseID));
 }
 
 StatusType LeaveCourse(void *DS, int studentID, int courseID){
+    if (DS == NULL){
+        return INVALID_INPUT;
+    }
     return (((Statistics*)DS)->LeaveCourse(studentID, courseID));
 }
 
 StatusType EnlargeCourse(void *DS, int courseID, int extrasize){
+    if (DS == NULL){
+        return INVALID_INPUT;
+    }
     return (((Statistics*)DS)->EnlargeCourse(courseID, extrasize));
 }
 
 StatusType GetAllCourses(void *DS, int ***courses, int**coursesSize, int *numOfCourses){
+    if (DS == NULL){
+        return INVALID_INPUT;
+    }
     return (((Statistics*)DS)->GetAllCourses(courses, coursesSize, numOfCourses));
 }
 
